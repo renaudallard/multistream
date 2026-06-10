fa1b9a4c752f7b2aa065daa4d9b33a195a388c65
Renaud Allard <renaud@allard.it>
2026-06-10 22:44:41 +0200

    Bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "it.allard.multistream"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.0"
+        versionCode = 11
+        versionName = "0.2.1"
     }
 
     signingConfigs {
