e9f776e06bb87e98608eb06290ad42cd5ca75cc6
Renaud Allard <renaud@allard.it>
2026-06-12 13:34:59 +0200

    Bump version to 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "it.allard.multistream"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.2.5"
+        versionCode = 16
+        versionName = "0.2.6"
     }
 
     signingConfigs {
